4700430f0e825d2e59ba8443a8bd6eb36f5461da
george-scott-code <george.scott.code@gmail.com>
2023-07-22 13:20:35 +0100

    display an image
    🎧 Burial x Four Tet - Moth | Four Tet |

--- src/image_proc/src/main.rs
+++ src/image_proc/src/main.rs
@@ -1,3 +1,24 @@
+use image::{DynamicImage, ImageFormat};
+use open;
+
 fn main() {
-    println!("Hello, world!");
+    // Path to the image file you want to display
+    let image_path = "../images/new_medieval.jpg";
+
+    // Load the image using the image crate
+    let img = image::open(image_path).expect("Failed to load image");
+
+    // Display the image using an external viewer (default image viewer on your system)
+    display_image(img);
+}
+
+fn display_image(image: DynamicImage) {
+    // Create a temporary file to save the image
+    let temp_path = "temp_image.png";
+    image.save(temp_path).expect("Failed to save temporary image");
+
+    // Try opening the default image viewer to display the image
+    if let Err(e) = open::that(temp_path) {
+        println!("Error opening the image viewer: {}", e);
+    }
 }
