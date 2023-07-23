2a842b04042bb4b79d6a2606d309d20b6dddeccd
george-scott-code <george.scott.code@gmail.com>
2023-07-23 13:11:55 +0100

    example of image operation -> overlay gradient
    🎧 Track 13 | J Dilla | Another Batch

--- src/image_proc/src/main.rs
+++ src/image_proc/src/main.rs
@@ -1,5 +1,7 @@
 use image::{DynamicImage, ImageFormat};
 use image::imageops::blur;
+use image::{Rgba, RgbaImage, Pixel};
+use image::imageops::overlay;
 use open;
 
 fn main() {
@@ -9,10 +11,22 @@ fn main() {
     // Load the image using the image crate
     let img = image::open(image_path).expect("Failed to load image");
 
-    let blurred = blur(&img, 2.5);
+    let mut blurred = blur(&img, 2.5);
     
     let temp_path = "temp_image.png";
+    // blurred.save(temp_path).expect("Failed to save temporary image");
+
+
+    let mut img2 = RgbaImage::new(1000, 1000);
+    let start = Rgba::from_slice(&[255, 73, 108, 0]);
+    let end = Rgba::from_slice(&[0, 0, 0, 255]);
+
+    image::imageops::horizontal_gradient(&mut img2, start, end);
+
+    overlay(&mut blurred, &img2, 0, 0);
+
     blurred.save(temp_path).expect("Failed to save temporary image");
+
     if let Err(e) = open::that(temp_path) {
         println!("Error opening the image viewer: {}", e);
     }
