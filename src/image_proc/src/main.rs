c15cbca6b0d9745205198c23c8ca711fa16b17ea
george-scott-code <george.scott.code@gmail.com>
2023-07-28 14:41:22 +0100

    remove blur tidy comments

--- src/image_proc/src/main.rs
+++ src/image_proc/src/main.rs
@@ -9,13 +9,11 @@ fn main() {
     let image_path = "../images/new_medieval.jpg";
 
     // Load the image using the image crate
-    let img = image::open(image_path).expect("Failed to load image");
+    let mut img = image::open(image_path).expect("Failed to load image");
 
     let mut blurred = blur(&img, 2.5);
     
     let temp_path = "temp_image.png";
-    // blurred.save(temp_path).expect("Failed to save temporary image");
-
 
     let mut img2 = RgbaImage::new(1000, 1000);
     let start = Rgba::from_slice(&[255, 73, 108, 0]);
@@ -23,15 +21,13 @@ fn main() {
 
     image::imageops::horizontal_gradient(&mut img2, start, end);
 
-    overlay(&mut blurred, &img2, 0, 0);
+    overlay(&mut img, &img2, 0, 0);
 
-    blurred.save(temp_path).expect("Failed to save temporary image");
+    img.save(temp_path).expect("Failed to save temporary image");
 
     if let Err(e) = open::that(temp_path) {
         println!("Error opening the image viewer: {}", e);
     }
-    // Display the image using an external viewer (default image viewer on your system)
-    // display_image(blurred);
 }
 
 fn display_image(image: DynamicImage) {
