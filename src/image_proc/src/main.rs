467a22f1ab61271aff4d1c9bf89542db7189f443
george-scott-code <george.scott.code@gmail.com>
2023-08-04 18:19:08 +0100

    remove unused

--- src/image_proc/src/main.rs
+++ src/image_proc/src/main.rs
@@ -8,14 +8,6 @@ fn main() {
     // Load the image using the image crate
     let mut img = image::open(image_path).expect("Failed to load image");
     let temp_path = "temp_image.png";
-    // let mut blurred = blur(&img, 2.5);
-
-    // let mut img2 = RgbaImage::new(img.width(), img.height());
-    // let start = Rgba::from_slice(&[255, 73, 108, 0]);
-    // let end = Rgba::from_slice(&[0, 0, 0, 255]);
-    // image::imageops::horizontal_gradient(&mut img2, start, end);
-
-    // overlay(&mut img, &img2, 0, 0);
     let mut rgba = img.to_rgba8();
     const CLEARPIXEL: image::Rgba<u8> = image::Rgba([0,0,0,40]);
 
@@ -27,17 +19,6 @@ fn main() {
 
     rgba.save(temp_path).expect("Failed to save temporary image");
 
-    if let Err(e) = open::that(temp_path) {
-        println!("Error opening the image viewer: {}", e);
-    }
-}
-
-fn display_image(image: DynamicImage) {
-    // Create a temporary file to save the image
-    let temp_path = "temp_image.png";
-    image.save(temp_path).expect("Failed to save temporary image");
-
-    // Try opening the default image viewer to display the image
     if let Err(e) = open::that(temp_path) {
         println!("Error opening the image viewer: {}", e);
     }
